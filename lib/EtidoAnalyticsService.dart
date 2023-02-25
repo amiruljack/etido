e7047e839082f3816d70fa5315932f4989cfc3a6
amiruljack <amirularifar@gmail.com>
2023-02-26 07:01:13 +0800

    Update EtidoAnalyticsService.dart

--- lib/EtidoAnalyticsService.dart
+++ lib/EtidoAnalyticsService.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 enum EtidoEvents {
-  pageLoad,
-  openApp,
-  buttonClick,
-  newTodo,
-  editTodo,
-  changeStatusToInProgress,
-  changeStatusToComplete,
+  pageLoad, // event when a page loads
+  openApp, // event when the app is opened
+  buttonClick, // event when a button is clicked
+  newTodo, // event when a new To Do is added
+  editTodo, // event when a To Do is edited
+  changeStatusToInProgress, // event when a To Do's status is changed to "in progress"
+  changeStatusToComplete, // event when a To Do's status is changed to "complete"
 }
 
 class EtidoAnalyticsService {
